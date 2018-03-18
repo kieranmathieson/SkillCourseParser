1383891f260fc7689738fb111cf03ab4ad9b613e
Kieran Mathieson <mathieso@oakland.edu>
2018-03-18 15:32:34 -0400

    Useful exception.

---
+++ src/Exception/SkillParserException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kieran
+ * Date: 3/17/2018
+ * Time: 11:09 AM
+ */
+
+namespace Drupal\hello\Exception;
+
+
+class SkillParserException extends \RuntimeException {
+
+}
